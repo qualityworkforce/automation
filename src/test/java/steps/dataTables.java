a80be0066ff203fb27271306e574ef254e4b7626
Ambi Chaliki <achaliki@financialforce.com>
2019-03-19 12:17:07 +0000

    added more data table scenarios

--- src/test/java/steps/dataTables.java
+++ src/test/java/steps/dataTables.java
@@ -10,49 +10,43 @@ import java.util.Map;
 public class dataTables {
 
     @Given("As a user I want to use data tables")
-    public void as_a_user_I_want_to_use_data_tables()
-    {
+    public void as_a_user_I_want_to_use_data_tables() {
         System.out.println("As a user I want to use data tables");
     }
 
     @When("I enter below data")
-    public void i_enter_below_data(io.cucumber.datatable.DataTable dataTable)
-    {
+    public void i_enter_below_data(io.cucumber.datatable.DataTable dataTable) {
 
-        List<Map<String, String>> fieldValues = dataTable.asMaps(String.class,String.class);
-        for (int i = 0 ; i <fieldValues.size() ;i++ )
-        {
+        List<Map<String, String>> fieldValues = dataTable.asMaps(String.class, String.class);
+        for (int i = 0; i < fieldValues.size(); i++) {
             System.out.println("First Name " + fieldValues.get(i).get("First Name"));
             System.out.println("Last Name " + fieldValues.get(i).get("Last Name"));
-            System.out.println("Place of Birth " +fieldValues.get(i).get("Place of Birth"));
+            System.out.println("Place of Birth " + fieldValues.get(i).get("Place of Birth"));
             System.out.println("Above field data is entered  ");
         }
 
     }
 
     @Then("it should be displayed on the screen")
-    public void it_should_be_displayed_on_the_screen()
-    {
+    public void it_should_be_displayed_on_the_screen() {
         System.out.println("Names displayed on the screen");
     }
 
     @Given("I am on Contact tab")
-    public void i_am_on_Contact_tab()
-    {
+    public void i_am_on_Contact_tab() {
         System.out.println("When I am on contact tab");
     }
 
     @Given("I click New button")
     public void i_click_New_button() {
-      System.out.println("Clicked on New button");
+        System.out.println("Clicked on New button");
     }
 
     @Given("I enter")
     public void i_enter(io.cucumber.datatable.DataTable dataTable) {
 
-        List<Map<String ,String >> fieldValues = dataTable.asMaps(String.class,String.class);
-        for (int i=0 ; i < fieldValues.size() ;i ++)
-        {
+        List<Map<String, String>> fieldValues = dataTable.asMaps(String.class, String.class);
+        for (int i = 0; i < fieldValues.size(); i++) {
             System.out.println("Last Name " + fieldValues.get(i).get("Last Name"));
             System.out.println("Contact Currency " + fieldValues.get(i).get("Contact Currency"));
             System.out.println("Is Resource " + fieldValues.get(i).get("Is Resource"));
@@ -61,21 +55,33 @@ public class dataTables {
     }
 
     @When("I click Save")
-    public void i_click_Save()
-    {
+    public void i_click_Save() {
         System.out.println("Save Button Clicked");
     }
 
     @Then("new Contact should be created")
-    public void new_Contact_should_be_created()
-    {
+    public void new_Contact_should_be_created() {
         System.out.println("New contact must be created ");
     }
 
     @Then("User must be navigated to newly created contact page")
-    public void user_must_be_navigated_to_newly_created_contact_page()
-    {
+    public void user_must_be_navigated_to_newly_created_contact_page() {
         System.out.println("User navigated to contact page");
+    }
+    @Given("I enter below")
+    public void i_enter_below(io.cucumber.datatable.DataTable dataTable) {
+
+        List<Map<String, String>> fieldValues = dataTable.asMaps(String.class, String.class);
+        for (int i = 0; i < fieldValues.size(); i++) {
+            System.out.println("New Record DATA");
+            System.out.println("First Name " + fieldValues.get(i).get("First Name"));
+            System.out.println("Last Name " + fieldValues.get(i).get("Last Name"));
+            System.out.println("Region " + fieldValues.get(i).get("Region"));
+            System.out.println("Resource Role " + fieldValues.get(i).get("Resource Role"));
+            System.out.println("Is Resource Active " + fieldValues.get(i).get("Is Resource Active"));
+            System.out.println("Is Resource " + fieldValues.get(i).get("Is Resource"));
+            System.out.println("Work Calender " + fieldValues.get(i).get("Work Calendar"));
+        }
     }
 
 }
