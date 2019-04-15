548b61caf633544bdac028f311a8bbce561e1943
Ambi Chaliki <achaliki@financialforce.com>
2019-04-15 21:19:17 +0100

    Added invalid scenario

--- src/test/java/steps/Php_Registration.java
+++ src/test/java/steps/Php_Registration.java
@@ -1,4 +1,5 @@
 package steps;
+
 import com.github.javafaker.Faker;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
@@ -6,8 +7,10 @@ import cucumber.api.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+
 import java.util.List;
 import java.util.Map;
+
 import static java.lang.Thread.currentThread;
 import static org.junit.Assert.assertEquals;
 
@@ -20,8 +23,9 @@ public class Php_Registration {
         System.out.println("Navigated to registration page ");
         hooks.driver.manage().window().maximize();
     }
+
     @Given("I enter valid details to below fields")
-    public void i_enter_valid_details_to_below_fields(io.cucumber.datatable.DataTable dataTable) throws InterruptedException {
+    public void valid(io.cucumber.datatable.DataTable dataTable) throws InterruptedException {
         List<Map<String, String>> fieldValues = dataTable.asMaps(String.class, String.class);
         for (int i = 0; i < fieldValues.size(); i++) {
             hooks.driver.findElement(By.name("firstname")).sendKeys(fieldValues.get(0).get("First Name"));
@@ -34,10 +38,26 @@ public class Php_Registration {
             currentThread().sleep(10000);
         }
         Faker faker = new Faker();
-        String email=faker.firstName();
-        hooks.driver.findElement(By.name("email")).sendKeys(email +"@gmail.com");
+        String email = faker.firstName();
+        hooks.driver.findElement(By.name("email")).sendKeys(email + "@gmail.com");
 
     }
+
+    @Given("I enter invalid details to below fields")
+    public void invalid(io.cucumber.datatable.DataTable dataTable) throws InterruptedException {
+        List<Map<String, String>> fieldValues = dataTable.asMaps(String.class, String.class);
+        for (int i = 0; i < fieldValues.size(); i++) {
+            hooks.driver.findElement(By.name("firstname")).sendKeys(fieldValues.get(0).get("First Name"));
+            hooks.driver.findElement(By.name("lastname")).sendKeys(fieldValues.get(0).get("Last Name"));
+            hooks.driver.findElement(By.name("phone")).sendKeys(fieldValues.get(0).get("Mobile Number"));
+            hooks.driver.findElement(By.name("email")).sendKeys(fieldValues.get(0).get("Email"));
+            hooks.driver.findElement(By.name("password")).sendKeys(fieldValues.get(0).get("Password"));
+            hooks.driver.findElement(By.name("confirmpassword")).sendKeys(fieldValues.get(0).get("Confirm Password"));
+            System.out.println("All Data is entered");
+            currentThread().sleep(10000);
+        }
+    }
+
     @When("I click Sign UP button")
     public void i_click_Sign_UP_button() throws InterruptedException {
         WebElement element = hooks.driver.findElement(By.xpath("//button[contains(text(),'Sign Up')]"));
@@ -45,6 +65,7 @@ public class Php_Registration {
         System.out.println("Clicked on Sign up button");
         currentThread().sleep(10000);
     }
+
     @Then("new account must be created")
     public void new_account_must_be_created() {
         String url = hooks.driver.getCurrentUrl();
@@ -52,10 +73,22 @@ public class Php_Registration {
         assertEquals("https://www.phptravels.net/account/", url);
         System.out.println("New Account Created Successfully ");
     }
+
     @Then("User must be navigated to newly created account page")
     public void user_must_be_navigated_to_newly_created_account_page() {
         String userName = hooks.driver.findElement(By.xpath("//h3[text()='Hi, Test4 User4']")).getText();
         assertEquals("Hi, Test4 User4", userName);
         System.out.println("Navigated to newly created account page ");
+    }
+
+    @Then("I should get a error message")
+    public void errorMessage() {
+        String error = hooks.driver.findElement(By.xpath("//div[@class='alert alert-danger']")).getText();
+        assertEquals("Email Already Exists.", error);
+    }
+
+    @Then("New account must not be created")
+    public void noAccountCreated() {
+
     }
 }
--- src/test/resources/features/accounts/php_registration.feature
+++ src/test/resources/features/accounts/php_registration.feature
@@ -3,11 +3,18 @@ Feature: Registration
   Scenario:Registration with valid data
     Given I navigate to PHP Travels Sing up page
     And  I enter valid details to below fields
-      | First Name | Last Name | Mobile Number | Email              | Password | Confirm Password |
-      | Test4       | User4     | 9898989898    | ambi5@gmail.com | Test1234 | Test1234         |
+      | First Name | Last Name | Mobile Number | Email           | Password | Confirm Password |
+      | Test4      | User4     | 9898989898    | ambi5@gmail.com | Test1234 | Test1234         |
     When I click Sign UP button
     Then new account must be created
     And User must be navigated to newly created account page
 
 
-
+  Scenario:Registration with already registered email
+    Given I navigate to PHP Travels Sing up page
+    And  I enter invalid details to below fields
+      | First Name | Last Name | Mobile Number | Email           | Password | Confirm Password |
+      | Test4      | User4     | 9898989898    | ambi5@gmail.com | Test1234 | Test1234         |
+    When I click Sign UP button
+    Then I should get a error message
+    And New account must not be created
