c2cb0e75593cfed40063c85ca3a4a698f73afa42
Ambi Chaliki <achaliki@financialforce.com>
2019-04-16 08:33:03 +0100

    moved driveres to a folder

--- src/test/java/steps/hooks.java
+++ src/test/java/steps/hooks.java
@@ -9,26 +9,17 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class hooks {
 
     public static WebDriver driver;
-
     @Before
     public void beforeScenarios() {
-       System.setProperty("webdriver.chrome.driver", "/Users/ambichaliki/Documents/automation/src/main/resources/chromedriver");
-       driver = new ChromeDriver();
+        System.setProperty("webdriver.chrome.driver", "/Users/ambichaliki/Documents/automation/src/main/resources/drivers/chromedriver");
+        driver = new ChromeDriver();
         //driver.get("https://login.salesforce.com/?locale=uk");
         //driver.get("https://www.eenadu.net/");
         driver.get("https://www.phptravels.net");
-
-//        System.setProperty("webdriver.gecko.driver", "/Users/ambichaliki/Documents/automation/src/main/resources/geckodriver");
-//         driver = new FirefoxDriver();
-//         driver.get("https://eenadu.net");
-//        System.out.println("Navigated to Eenadu successfully  ");
-
     }
-
     @After
     public void afterScenarios() {
-
-       driver.quit();
+        driver.quit();
         System.out.println("After Scenario-Closed browser successfully");
     }
 }
